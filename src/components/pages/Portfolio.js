98569efe5734bf2474000def95e6c27addef6a21
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-27 20:57:44 -0700

    Addred resume JSX and styles

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Project from "../Project";
+import Project from "./Project";
 import "../styles/Portfolio.css";
 
 //PORTFOLIO
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "../styles/Resume.css";
 //RESUME
     //Downloadable resume and list of developer's proficiencies
 
 function Resume() {
+
     return (
-        
+        <>
+        <div className="center-align">
+            <a href="images/paolaG_resume.png" download="Paola's Resume" id="download"> Click Here to Download Resume </a>
+        </div>
+        <div className="center-align">
+            <img src="images/paolaG_resume.png" className="responsive-img"/>
+        </div>
+        </>
     )
 }
 
