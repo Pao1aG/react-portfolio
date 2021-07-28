b7946451bda8a25dfa9e281085d794685a185160
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-27 19:45:01 -0700

    added Footer JSX and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import "./styles/Footer.css";
 
 //Text or icon links to my GitHub, LinkedIn, and third profile platform (Stack Overflow, twitter, etc)
 
 function Footer() {
+    return(
+        <footer className="page-footer pink darken-4">
+        <div className="container">
+            <ul className="center-align flexLinks">
+                <li><a className="grey-text text-lighten-3" href="#!">GitHub</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">LinkedIn</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Facebook</a></li>
+            </ul>
+        </div>
+
+        <div className="footer-copyright">
+          <div className="container center-align">
+          © 2014 Copyright Text
+          </div>
+        </div>
+      </footer>
+    )
 
 }
 
