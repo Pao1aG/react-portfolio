2a691ca8fd7b32cc5a44e7be6c70645871a8d073
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-27 20:23:49 -0700

    added Contact JSX and styles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,17 +5,17 @@ import React from 'react';
 
 function About() {
     return (
-        <div class="container aboutMe">
-            <div class="row">
-                <div class="col m6 s12">
-                    <img src="images/profile-pic.jpg" class="circle responsive-img"/>
+        <div className="container aboutMe">
+            <div className="row">
+                <div className="col m6 s12">
+                    <img src="images/profile-pic.jpg" className="circle responsive-img"/>
                 </div>
-                <div class="col m6 s12">
-                    <p class="flow-text">Welcome to my Portfolio! My name is Paola Gonzalez and I am currently a student of the Trilogy Coding Bootcamp through the University of Arizona (UArizona).</p>
-                    <p class="flow-text">Before I took the step to delve into the exciting world of coding, I studied public health at UArizona earning both a Bachelor of Science and a Masters degree. </p>
-                    <p class="flow-text"> Throughout my career, I have served my community through projects that utilized my skills in organization and creativity. When I worked as a Community Nutrition Educator, I designed educational materials to teach over Zoom and co-directed, filmed, and edited videos for our participants. I have also managed and maintained the social media and outreach materials of different organizations. </p>
-                    <p class="flow-text">All roads have lead me here... The time spent researching, designing, and collaborating with people of many different backgrounds has inspired me to take on coding projects such as the ones you will see here.</p>
-                    <p class="flow-text">I will keep this page updated with new projects. Do visit again soon!</p>
+                <div className="col m6 s12">
+                    <p className="flow-text">Welcome to my Portfolio! My name is Paola Gonzalez and I am currently a student of the Trilogy Coding Bootcamp through the University of Arizona (UArizona).</p>
+                    <p className="flow-text">Before I took the step to delve into the exciting world of coding, I studied public health at UArizona earning both a Bachelor of Science and a Masters degree. </p>
+                    <p className="flow-text"> Throughout my career, I have served my community through projects that utilized my skills in organization and creativity. When I worked as a Community Nutrition Educator, I designed educational materials to teach over Zoom and co-directed, filmed, and edited videos for our participants. I have also managed and maintained the social media and outreach materials of different organizations. </p>
+                    <p className="flow-text">All roads have lead me here... The time spent researching, designing, and collaborating with people of many different backgrounds has inspired me to take on coding projects such as the ones you will see here.</p>
+                    <p className="flow-text">I will keep this page updated with new projects. Do visit again soon!</p>
                 </div>
             </div>
         </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,42 @@
+import React from "react";
 //CONTACT ME
     // Contact form with fields for a name, email address, and message
         //Notification that field is required if cursor is moved without entering text
         //Notification if invalid email address is entered
+
+function Contact() {
+    return (
+        <div className="container contactForm">
+        <h4>Contact Me</h4>
+        <div className="row">
+            <form className="col s12">
+              <div className="row">
+                <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                  <input placeholder="Name" id="first_name" type="text" className="validate"/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                <i className="material-icons prefix">email</i>
+                  <input placeholder="Email" id="email" type="email" className="validate"/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                <i className="material-icons prefix">message</i>
+                  <textarea placeholder="Your Message" id="message" className="materialize-textarea"></textarea>
+                </div>
+              </div>
+              <div className="center-align">
+                  <button type="submit" className="btn waves-effect pink darken-3">Submit
+                  <i className="material-icons right">send</i>
+                  </button>
+              </div>
+            </form>
+          </div>
+        </div>
+    )
+}
+
+export default Contact;
