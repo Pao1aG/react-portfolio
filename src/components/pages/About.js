bccdd6965d0dae24010c2e5c8a47a0f4067f2106
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-08-19 14:45:16 -0700

    Polishing Contact Me section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,9 +12,9 @@ function About() {
                     <img src="images/profile-pic.jpg" className="responsive-img" alt=""/>
                 </div>
                 <div className="col m6 s12">
-                    <p className="flow-text">Welcome to my Portfolio! My name is Paola Gonzalez and I am currently a student of the Trilogy Coding Bootcamp through the University of Arizona (UArizona).</p>
+                    <p className="flow-text">Welcome to my Portfolio! My name is Paola Gonzalez and I am a graduate of the Trilogy Coding Bootcamp through the University of Arizona (UArizona).</p>
                     <p className="flow-text">Before I took the step to delve into the exciting world of coding, I studied public health at UArizona earning both a Bachelor of Science and a Masters degree. </p>
-                    <p className="flow-text"> Throughout my career, I have served my community through projects that utilized my skills in organization and creativity. When I worked as a Community Nutrition Educator, I designed educational materials to teach over Zoom and co-directed, filmed, and edited videos for our participants. I have also managed and maintained the social media and outreach materials of different organizations. </p>
+                    <p className="flow-text"> Throughout my career, I have served my community through projects that utilized my skills in organization and creativity. When I worked as a Community Nutrition Educator, I designed educational materials to teach over Zoom and co-directed, filmed, and edited videos for our participants. I have also managed and maintained the social media and online outreach materials of different organizations. </p>
                     <p className="flow-text">All roads have lead me here... The time spent researching, designing, and collaborating with people of many different backgrounds has inspired me to take on coding projects such as the ones you will see here.</p>
                     <p className="flow-text">I will keep this page updated with new projects. Do visit again soon!</p>
                 </div>
--- src/components/pages/ContactInfo.js
+++ src/components/pages/ContactInfo.js
@@ -1,9 +1,46 @@
 import React from "react";
+import "../styles/ContactInfo.css";
 
 function ContactInfo() {
 
     return(
-        <h1>This is a test</h1>
+        <div className="container">
+             <div class="col s12 m7">
+                <h4>Email</h4>
+                <div class="card horizontal brown lighten-5">
+                    <div class="card-image">
+                        {/* <img src="images/email_contact.png" alt=""/> */}
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content center-align">
+                            <p>paolaagonzalezm@email.arizona.edu</p>
+                            <p>paolaagonzalezm@gmail.com</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="mailto:paolaagonzalezm@email.arizona.edu" target="_blank" rel="noreferrer">Email Me</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <div class="col s12 m7">
+                <h4>Professional Media</h4>
+                <div class="card horizontal brown lighten-5">
+                    <div class="card-image">
+                        {/* <img src="images/github_contact.png" alt=""/> */}
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content center-align">
+                            <p>GitHub: Pao1aG</p>
+                            <p>LinkedIn: /paolaagonzalezm </p>
+                        </div>
+                        <div class="card-action">
+                            <a  href="https://www.github.com/Pao1aG/" target="_blank" rel="noreferrer">GitHub</a>
+                            <a  href="https://www.linkedin.com/in/paolaagonzalezm/" target="_blank" rel="noreferrer">LinkedIn</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         )
 }
 
